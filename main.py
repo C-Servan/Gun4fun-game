54d3f2403c45fce17b854610e75988c12cc69329
C-Servan <gun4fun.soporte@gmail.com>
2026-04-17 13:05:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,24 @@ import telebot
 from flask import Flask
 from threading import Thread
 
-# Configuración del servidor web para Render
 app = Flask('')
+
 @app.route('/')
 def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
-# Configuración del Bot
 TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "¡Hola! Soy Gun4fun-game. Estoy listo para los juegos.")
+    bot.reply_to(message, "¡Hola! Soy Gun4fun-game. Estoy listo.")
 
-if __name__ == "__main__":
-    # Arranca el servidor web en un hilo aparte
+if name == "__main__":
     t = Thread(target=run)
     t.start()
-    # Arranca el bot
     bot.polling(none_stop=True)
